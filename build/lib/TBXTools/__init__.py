cf71d160e9f3701a4ae6b6d6521ed3ebc3fa57c0
martacozzini2 <marta.01ts@gmail.com>
2026-05-15 07:57:35 +0000

    new tokenization

--- build/lib/TBXTools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .core import Extractor
-# from .preprocessor import Preprocessor
-from .extractor import StatisticalExtractor, BertExtractor
-
-__all__=[
-    "Extractor"
-    # "StatisticalExtractor",
-    # "Preprocessor"
-    ]
--- build/lib/TBXTools/extractor/base.py
+++
@@ -1,9 +0,0 @@
-class BaseExtractor:
-    def preprocess(self, corpus, **kwargs):
-        raise NotImplementedError
-
-    def extract(self, segments, **kwargs):
-        raise NotImplementedError
-
-    def postprocess(self, terms, **kwargs):
-        raise NotImplementedError
