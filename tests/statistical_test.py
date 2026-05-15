98bd0d1b91c95cdc3e171de01bd6a91f0c2e3725
gon-zalo <gonzsan02@proton.me>
2026-05-15 08:51:04 +0000

    test file fix

--- tests/statistical_test.py
+++ tests/statistical_test.py
@@ -4,17 +4,13 @@ extractor = Extractor(
     method=StatisticalExtractor(
         nmin=2,
         nmax=3),
-    project_name="prova_case_normalization_8",
-    corpus="Mental_disorder.txt",
-    language= "english"
+    project_name="test",
+    corpus="Mental-health.txt",
+    language="english"
 )
 
 results = extractor.extract(case_normalization=True, verbose=False)
 
-#results.nest_normalization()
-#results.regex_exclusion()
-#results.save_candidates("test.txt")
-
-print(results.tokens(limit=50))
-
-
+results.nest_normalization()
+results.regex_exclusion()
+results.save_candidates("test.txt")
