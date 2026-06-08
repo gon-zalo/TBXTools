9392752a2a03d80f73f753187665e585394da713
martacozzini2 <marta.01ts@gmail.com>
2026-06-08 09:11:26 +0000

    new_test

--- tests/new_linguistic_test.py
+++ tests/new_linguistic_test.py
@@ -2,19 +2,14 @@ from TBXTools import Extractor, LinguisticExtractor
 
 
 extractor = Extractor(
-    methodology=LinguisticExtractor(nmin=2, nmax=3, input_is_tagged= True, linguistic_patterns= "ling_pat-en.txt"),
-    project_name="prova_argomenti_diversi_tagged_e_patterns",
-    corpus="mental_ling.txt",
+    methodology=LinguisticExtractor(nmin=2, nmax=3, input_is_tagged= False, evaluation_terms= "evaluation_terms.txt"),
+    project_name="prova",
+    corpus="Mental_disorder.txt",
     language="english",
     overwrite_project=True
 )
 
-results = extractor.extract(case_normalization=False, verbose=False)
+results = extractor.extract(case_normalization=True, verbose=False)
 
 results.nest_normalization(verbose=False)
-results.regex_exclusion(verbose=False)
 results.save_candidates("test.txt")
-
-# Results can be inspected with the following methods:
-#print(f"\nTerms: {results.terms()}")
-#print(f"\nTagged Ngrams: {results.tagged_ngrams()}") 
