7d9415c3f497d6e0325124afe845a8afc8e1f5f5
martacozzini2 <marta.01ts@gmail.com>
2026-06-15 09:41:15 +0000

    test

--- tests/linguistic_test.py
+++ tests/linguistic_test.py
@@ -5,15 +5,15 @@ from TBXTools import Extractor, LinguisticExtractor
 methodology = LinguisticExtractor(
     nmin=2,
     nmax=3,
-    corpus_is_tagged=True,
-    evaluation_terms="evaluation_terms.txt"
+    corpus_is_tagged=False,
+    linguistic_patterns="ling_pat-ca.txt"
 )
 
 extractor = Extractor(
     methodology=methodology,
     project_name="prova",
-    corpus="tagged_corpus.txt",
-    language="english",
+    corpus="Trastorn_mental.txt",
+    language="catalan",
     overwrite_project=True
 )
 
