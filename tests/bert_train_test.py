3d1263b6d62513e7c8add70479aa3d9b5d4e7046
gon-zalo <gonzsan02@gmail.com>
2026-06-29 12:03:30 +0000

    models, corpora

--- tests/bert_train_test.py
+++ tests/bert_train_test.py
@@ -6,17 +6,25 @@ herbert = "allegro/herbert-base-cased"
 herbert_large = "allegro/herbert-large-cased"
 polbert = "dkleczek/bert-base-polish-cased-v1"
 polish_roberta = "sdadas/polish-roberta-base-v2"
+distilbert = "distilbert/distilbert-base-multilingual-cased"
+pl_distilbert = "Geotrend/distilbert-base-pl-cased"
+
+corpus = "corpus-wmt-pl.txt"
+corpus_small = "corpus-wmt-pl-small.txt"
+corpus_mini = "corpus-wmt-pl-mini.txt"
+corpus_nano = "corpus-wmt-pl-nano.txt"
 
 trainer = BertTrainer(
     project_name="bert-train-test",
-    corpus="corpus-wmt-pl.txt",
+    corpus=corpus_nano,
+    overwrite_project=False,
     model=polbert,
-    split=True, 
     external_terms='pl_iate.txt', 
     labels="bio",
+    split=False, 
     lr=5e-05,
     batch_size=16,
     epochs=6,
     weight_decay=0.03)
 
-trainer.train(save_as='wmt-termlgy-test-1')
+trainer.train(save_as='wmt-termlgy-test-polbert')
