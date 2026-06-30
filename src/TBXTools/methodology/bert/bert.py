7072c580d2401d89b286008cb06555f61ec352eb
gon-zalo <gonzsan02@gmail.com>
2026-06-30 08:30:34 +0000

    _processor

--- src/TBXTools/methodology/bert/bert.py
+++ src/TBXTools/methodology/bert/bert.py
@@ -12,7 +12,7 @@ class BertMethodology(BaseMethodology):
         self.name = "BertMethodology"
         self.model_name = model
 
-        self.processor = BertProcessor(model_name=self.model_name)
+        self._processor = BertProcessor(model_name=self.model_name)
         self.labels = labels.lower() if labels else None
 
         
@@ -20,42 +20,41 @@ class BertMethodology(BaseMethodology):
         from datasets import Dataset
         import numpy as np
 
-        self.processor.load_transformers()
-        tokens_output, tokenized_corpus_for_sqlite, dataframe = self.processor.preprocess(segments=segments, verbose=verbose)
-        labels = self.processor.choose_labels(self.labels)
+        self._processor.load_transformers()
+        tokens_output, tokenized_corpus_for_sqlite, dataframe = self._processor.preprocess(segments=segments, verbose=verbose)
+        labels = self._processor.choose_labels(self.labels)
         label2id = {l: i for i, l in enumerate(labels)}
         id2label = {i: l for l, i in label2id.items()}
 
         # need to check this nonsense
-        eval_hf = Dataset.from_pandas(dataframe)
-        prepared_data = self.processor.prepare_data(self.processor.tokenizer)
-        eval_data = eval_hf.map(prepared_data, batched=True)
+        dataframe = Dataset.from_pandas(dataframe)
+        eval_data = dataframe.map(self._processor.prepare_unlabeled_inputs, batched=True)
 
         # df = eval_data.to_pandas()
         # print(df.head())
         # df.to_csv("ins.csv", index=False)
 
         print("\nPredicting terms")
-        trainer = self.processor.trainer
+        trainer = self._processor.trainer
         prediction_logits, _, _ = trainer.predict(eval_data)
         predictions = np.argmax(prediction_logits, axis=2)
         predicted_tokens = []
         for i in range(len(eval_data)):
             tokens = eval_data[i]['tokenized_segment']
             predicted_ids = predictions[i]
-            reconstructed = self.processor.pred_labels_to_tokens(tokens, predicted_ids, id2label)
+            reconstructed = self._processor.pred_labels_to_tokens(tokens, predicted_ids, id2label)
             predicted_tokens.append(reconstructed)
 
         print("Predictions finalized")
 
         dataframe['predicted_tokens'] = predicted_tokens
-        dataframe['predicted_terms'] = dataframe['predicted_tokens'].apply(self.processor.merge_tokens)
+        dataframe['predicted_terms'] = dataframe['predicted_tokens'].apply(self._processor.merge_tokens)
 
         #check
         # dataframe.to_csv('./evaluation_dataframe.csv', index=False)
 
         predicted_terms = dataframe['predicted_terms'].tolist()
-        predicted_terms = self.processor.flatten_list(predicted_terms)
+        predicted_terms = self._processor.flatten_list(predicted_terms)
 
         candidate_terms = []
         term_counts = Counter(predicted_terms)
