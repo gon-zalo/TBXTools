6d9b6425b12e41f627fe82a393acb3529d71e432
martacozzini2 <marta.01ts@gmail.com>
2026-07-23 09:34:26 +0000

    new test added

---
+++ tests/test_tutorial_stat.py
@@ -0,0 +1,61 @@
+from TBXTools import Extractor
+from TBXTools.methodology import StatisticalMethodology
+
+regexes = [".+ health"]
+tsr_terms="tsr_terms.txt"
+
+extractor = Extractor(
+    methodology=StatisticalMethodology(
+        nmin=2,
+        nmax=3,
+        case_normalization=True
+    ),
+    project_name="statistical-example_tutorial",
+    corpus="mental_lungo.txt",
+    language="english",
+    overwrite_project=True,
+)
+
+results = extractor.extract(verbose=False)
+
+#results.nest_normalization(verbose=False)
+#results.regex_exclusion(regexes=regexes, verbose=False)
+#results.save_candidates("statistical-candidates.txt")
+
+# Results can be inspected with the following methods:
+all_terms = results.terms(limit=None)
+print(f"\nNumber of terms: {len(all_terms)}. Expected number: 223") 
+print(f"\nTerms: {results.terms()}")
+print(f"\nNgrams: {results.ngrams()}")
+print(f"\nTokens: {results.tokens()}") 
+
+# ----------
+# Scenario TSR
+print("")
+print("--- SCENARIO TSR ---\n")
+
+extractor = Extractor(
+    methodology=StatisticalMethodology(
+        nmin=2,
+        nmax=3,
+        case_normalization=True
+    ),
+    project_name="statistical-example-tsr_tutorial",
+    corpus="mental_lungo.txt",
+    language="english",
+    overwrite_project=True,
+)
+
+results = extractor.extract(verbose=False)
+
+#results.nest_normalization(verbose=True)
+#results.regex_exclusion(regexes=regexes, verbose=True)
+#results.tsr(tsr_terms=tsr_terms, type="flexible", max_iterations=10, verbose=False)
+#results.lemmatization(verbose=False)
+
+# Results can be inspected with the following methods:
+all_terms = results.terms(limit=None)
+print(f"\nNumber of terms: {len(all_terms)}. Expected number: 121") 
+print(f"\nTerms: {results.terms()}")
+print(f"\nNgrams: {results.ngrams()}")
+print(f"\nTokens: {results.tokens()}") 
