8426f8999860ecff661fd9d6ceb767cf0ba7acc5
gon-zalo <gonzsan02@proton.me>
2026-08-04 10:35:10 +0000

    removed unnecessary comments

--- src/TBXTools/methodology/bert/bert.py
+++ src/TBXTools/methodology/bert/bert.py
@@ -19,35 +19,30 @@ class BertMethodology(BaseMethodology):
         self.model_name = model
 
         self.labels = labels.lower() if labels else None
-        self.processor = BertProcessor(model_name=self.model_name, labels=self.labels)
+        self.processor = BertProcessor(model_name=self.model_name, labeling_scheme=self.labels)
 
         
-    def extract(self, segments, verbose, by_segment=False, lemmatize=True):
+    def extract(self, segments):
         '''
-        Extracts candidate terms using BERT. This methodology uses a previously fine-tuned model on automatically annotated data to predict labels for each token of the evaluation data.
+        Extracts candidate terms using BERT. This methodology uses a previously fine-tuned model on automatically annotated data to predict terms.
 
         Args:
             segments: A list of segments to process.
-            verbose (bool, optional): If True, enables detailed logging. Defaults to False.
-            by_segment (bool, optional): If True, outputs candidate terms grouped by segment.
         
         Returns:
             Results: An object containing the tokens, candidate terms. It also returns separately the tokenized corpus.
         '''
+        # verbose (bool, optional): If True, enables detailed logging. Defaults to False.
+        # by_segment (bool, optional): If True, outputs candidate terms grouped by segment.
+        
         from datasets import Dataset
         import numpy as np
 
         print(f'\nInitializing model:  {self.model_name}', flush=True)
         self.processor._load_model()
-        self.processor._load_tokenizer_and_data_collator()
+        self.processor._load_trainer()
 
-
-        dataframe = self.processor.preprocess_eval(segments=segments, lemmatize=lemmatize)
-        
-        # labels = self.processor.choose_labels(self.labels)
-        # label2id = {l: i for i, l in enumerate(labels)}
-        # id2label = {i: l for l, i in label2id.items()}
-
+        dataframe = self.processor.preprocess_test(segments=segments)
         eval_data = Dataset.from_pandas(dataframe)
 
         print("\nPredicting terms")
@@ -56,39 +51,31 @@ class BertMethodology(BaseMethodology):
         predictions = np.argmax(prediction_logits, axis=2)
         predicted_terms = []
                 
-        if lemmatize:
-            for i in range(len(eval_data)):
-                tokens = eval_data[i]['tokens']
-                predicted_ids = predictions[i]
-                reconstructed = self.processor._bio_to_terms(
-                    tokens=tokens,
-                    labels=predicted_ids)
+        for i in range(len(eval_data)):
+            tokens = eval_data[i]['tokens']
+            predicted_ids = predictions[i]
+            reconstructed = self.processor._bio_to_terms(
+                tokens=tokens,
+                labels=predicted_ids)
 
-                predicted_terms.append(reconstructed)
-
+            predicted_terms.append(reconstructed)
 
         clean_terms = self.processor.process_predictions(predicted_terms)
 
-        # print(clean_terms)
-        dataframe['predicted_terms'] = clean_terms # here they are organized by segment, no?
-        #check
-        # dataframe.to_csv('./evaluation_dataframe.csv', index=False)
+        dataframe['predicted_terms'] = clean_terms
 
         #i dont remember why im doing this, but keep for now, otherwise it wont work
-        clean_terms = dataframe['predicted_terms'].tolist() # this line i can remove i think
+        clean_terms = dataframe['predicted_terms'].tolist()
         clean_terms = self.processor._flatten_list(clean_terms)
 
-        # print(clean_terms)
         # output for tbxtools, calculating count of each term
         candidate_terms = []
         term_counts = Counter(clean_terms)
         term_counts = dict(sorted(term_counts.items(), key=lambda item: item[1], reverse=True))
         for term, count in term_counts.items():
-            if term == "—":
-                print(term)
             if term:
                 n = len(term.split(" "))
                 candidate_terms.append((term, n, "count", count))
         tokenized_segments = []
-        # print(candidate_terms)
+
         return Results(terms=candidate_terms), tokenized_segments  
