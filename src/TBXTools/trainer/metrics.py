273547d9095ec93a93bc603f03c716d2704d472a
gon-zalo <gonzsan02@proton.me>
2026-08-04 10:35:48 +0000

    removed old metrics

--- src/TBXTools/trainer/metrics.py
+++ src/TBXTools/trainer/metrics.py
@@ -20,58 +20,6 @@ class Metrics:
 
         return precision, recall, f1
 
-    def compute_metrics(self, p):
-        import numpy as np
-        import pandas as pd
-        prediction_logits, label_ids = p # label_ids are true padded label IDs from eval_data
-        predictions = np.argmax(prediction_logits, axis=2)
-
-        all_pred_terms = []
-        all_true_terms = []
-        rows = []
-
-        for i in range(len(self.eval_data)):
-            tokens = self.eval_data[i]["tokens"]
-            text = self.eval_data[i]["text"]
-            offsets = self.eval_data[i]["offset_mapping"]
-            predicted_ids = predictions[i]
-            true_ids = label_ids[i]
-
-            # for j, (token, true_id, pred_id) in enumerate(zip(tokens, true_ids, predicted_ids)):
-            #     if token in ["[CLS]", "[SEP]", "[PAD]"] or true_id == -100:
-            #         continue
-                
-            #     rows.append({
-            #         "segment_id": i,
-            #         "token_index": j,
-            #         "token": token,
-            #         "true_label": self.processor._id2label[true_id],
-            #         "pred_label": self.processor._id2label[pred_id],
-            #         "correct": true_id == pred_id
-            #     })
-
-            reconstructed_predicted_terms = self.processor._pred_labels_to_text(text, offsets, predicted_ids, self.processor._id2label)
-            all_pred_terms.extend(reconstructed_predicted_terms)
-
-            reconstructed_true_terms = self.processor._pred_labels_to_text(text, offsets, true_ids, self.processor._id2label)
-            all_true_terms.extend(reconstructed_true_terms)
-
-            # print(f"Segment {i}")
-            # print("pred terms in segment:", reconstructed_predicted_terms)
-            # print("true terms in segment:", reconstructed_true_terms)
-
-        # df = pd.DataFrame(rows)
-        # df.to_csv("debug_test.csv", index=False)
-
-        # print("\nPredicted terms that do not appear in the training corpus:")
-        # unique_preds = [term for term in all_pred_terms if term not in all_true_terms]
-        # print(unique_preds)
-        # print("All unique predicted terms")
-        # print(set(all_pred_terms))
-        precision, recall, f1 = self.score(all_pred_terms, all_true_terms)
-
-        return {"precision": precision, "recall": recall, "f1": f1}
-
     def _old_compute_metrics_lemm(self, p):
         import numpy as np
         import pandas as pd
@@ -144,7 +92,6 @@ class Metrics:
         precision, recall, f1 = self.score(pred_dict, true_dict)
 
         return {"precision": precision, "recall": recall, "f1": f1}
-
 
     def score(self, pred_dict, true_dict):
         tp = 0
