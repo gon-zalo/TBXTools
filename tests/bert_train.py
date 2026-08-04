a72012508371f5db85e5b6d70503af9c979ea2ea
gon-zalo <gonzsan02@proton.me>
2026-08-04 10:56:14 +0000

    updated test file

--- tests/bert_train.py
+++ tests/bert_train.py
@@ -1,24 +1,21 @@
 from TBXTools.trainer import BertTrainer
 
 xlm_roberta = "FacebookAI/xlm-roberta-base"
-# xlm_roberta_large = "FacebookAI/xlm-roberta-large"
-# roberta = "sdadas/polish-roberta-base-v2"
 
 trainer = BertTrainer(
     project_name="wmt-bert-train-allpositive-ate",
-    corpus="",
-    overwrite_project=False,
-    language="en",
-    model=xlm_roberta,
-    external_terms="nan", # code to handle this if everything is already in db 
-    labels="bio",
-    lr=4.5e-5,
-    batch_size=32,
-    epochs=1.7,
-    weight_decay=0.028,
-    warmup_ratio=0.016)
+    language="en")
 
-trainer.train(save_as="wmt-roberta-allpositive-2-ate", split=False, lemmatize=True, expand_labels=False, only_annotate=False)
+trainer.train(
+    model_name= xlm_roberta, 
+    save_as="wmt-roberta-allpositive-2-ate", 
+    split=False, 
+    expand_labels=False, 
+    lr=4.5e-5, 
+    batch_size=32, 
+    epochs=1.7, 
+    weight_decay=0.028, 
+    warmup_ratio=0.016)
 
 # parameters both lang, both domains
 # Parameters: lr = 4.52034259861242e-05, epochs = 5, batch_size = 16, weight_decay = 0.028099146321555947, warmup_ratio = 0.01668312545962061
