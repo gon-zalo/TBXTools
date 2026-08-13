125f30d190b41a5fbdb2a4c1738feac39defe400
gon-zalo <gonzsan02@proton.me>
2026-08-13 10:48:44 +0000

    spacy implemented in resources

--- src/TBXTools/_resources/resources.py
+++ src/TBXTools/_resources/resources.py
@@ -1,4 +1,5 @@
 from importlib import import_module
+import spacy
 
 class Resources:
     '''
@@ -7,14 +8,27 @@ class Resources:
     Args:
         lang_code: The language ISO code of the corpus language.
     '''
-    def __init__(self, lang_code):
+    def __init__(self, lang, lang_code):
+        self.lang = lang
         self.lang_code = lang_code
 
-    def fetch_stopwords(self):
-        module = import_module(f"TBXTools._resources.stopwords.{self.lang_code}")
-        return module.STOPWORDS
-
     def fetch_inner_stopwords(self):
-        module = import_module(f"TBXTools._resources.inner.{self.lang_code}")
-        return module.INNER_STOPWORDS
-    
+        try:
+            module = import_module(f"TBXTools._resources.inner.{self.lang_code}")
+
+            return module.INNER_STOPWORDS
+        
+        except ModuleNotFoundError:
+            return set()
+        
+    def get_spacy_stopwords(self):
+        '''
+        Fetches the spaCy stop words for a given language.
+        '''
+        try:
+            lang_class = spacy.util.get_lang_class(self.lang_code)
+            
+            return lang_class.Defaults.stop_words
+            
+        except ImportError:
+            raise ValueError(f"Cannot import stopwords because {self.lang} ({self.lang_code}) is not supported in spaCy. You must pass an external list of stopwords to Extractor using the 'stopwords' argument.")
